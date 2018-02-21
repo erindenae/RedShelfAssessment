875ebcd43e88d742e68ef95a8d945aa054f500a5
Erin <tkx748@Erin-Ds-MBP.am.mot-solutions.com>
2018-02-21 16:18:10 -0600

    Ember Component Exercise complete, left logs commented out

--- app/components/credit-card.js
+++ app/components/credit-card.js
@@ -20,7 +20,29 @@ export default Ember.Component.extend({
   isExpired: Ember.computed("card.expirationMonth", "card.expirationYear", function() {
     let card = this.get("card");
     let today = this.get("today");
-    return false;
+
+    //compare expiration year
+      if(card.expirationYear > today.getFullYear()) {
+        //console.log('not expired');
+        return false;
+      }
+      else if(card.expirationYear < today.getFullYear()) {
+        //console.log('card IS expired');
+        return true;
+      }
+
+    //compare expiration month for expirations in the current year
+      else {
+
+        if (card.expirationMonth > today.getMonth()) {
+          //console.log('month compared, card is not expired.');
+          return false;
+        }
+        else{
+         //console.log('month compared, card IS expired.'); 
+         return true;
+        }
+      }
   })
 /*
 
@@ -33,4 +55,5 @@ export default Ember.Component.extend({
   },
 
 */
+
 });
